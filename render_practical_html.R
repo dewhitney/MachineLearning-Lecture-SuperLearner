eeda21928d85b6cd5e069be14acb0390e96f6c16
David <daveewhitney@gmail.com>
2022-02-03 20:59:31 +0000

    rendering pdfs of practical

--- render_practical_html.R
+++ render_practical_html.R
@@ -15,3 +15,26 @@ rmarkdown::render("superlearner-practical-part2.Rmd",
 rmarkdown::render("superlearner-practical-part2.Rmd",
                   params = list(solutions = FALSE),
                   output_file="superlearner-practical-part2.html")
+
+## PDFs
+
+rmarkdown::render("superlearner-practical-part1.Rmd",
+                  output_format = 'pdf_document',
+                  params = list(solutions = TRUE),
+                  output_file="superlearner-solutions-part1.pdf")
+
+rmarkdown::render("superlearner-practical-part1.Rmd",
+                  output_format = 'pdf_document',
+                  params = list(solutions = FALSE),
+                  output_file="superlearner-practical-part1.pdf")
+
+rmarkdown::render("superlearner-practical-part2.Rmd",
+                  output_format = 'pdf_document',
+                  params = list(solutions = TRUE),
+                  output_file="superlearner-solutions-part2.pdf")
+
+rmarkdown::render("superlearner-practical-part2.Rmd",
+                  output_format = 'pdf_document',
+                  params = list(solutions = FALSE),
+                  output_file="superlearner-practical-part2.pdf")
+
