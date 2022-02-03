405a1107181474fc659e4e5a69f4e7750fe3be2f
David <daveewhitney@gmail.com>
2022-02-03 19:06:28 +0000

    rendering html for practical 1

---
+++ render_practical_html.R
@@ -0,0 +1,17 @@
+# Render the different versions of the practical we require
+
+rmarkdown::render("superlearner-practical-part1.Rmd",
+                  params = list(solutions = TRUE),
+                  output_file="superlearner-solutions-part1.html")
+
+rmarkdown::render("superlearner-practical-part1.Rmd",
+                  params = list(solutions = FALSE),
+                  output_file="superlearner-practical-part1.html")
+
+rmarkdown::render("superlearner-practical-part2.Rmd",
+                  params = list(solutions = TRUE),
+                  output_file="superlearner-solutions-part1.html")
+
+rmarkdown::render("superlearner-practical-part2.Rmd",
+                  params = list(solutions = FALSE),
+                  output_file="superlearner-practical-part1.html")
